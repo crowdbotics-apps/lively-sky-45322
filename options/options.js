c5bee8eccf2c1a4b02197f3d32366bbc273460c3
Crowdbotics <team@crowdbotics.com>
2024-01-08 20:05:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-sky-45322",
+  url: "https://lively-sky-45322.botics.co",
+  api: "https://lively-sky-45322.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
